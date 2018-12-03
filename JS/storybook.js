04f6e4a15ace74d58e655ae19dc61af1afb5343b
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 14:10:29 -0600

    updated storybook html cs and js

--- JS/storybook.js
+++ JS/storybook.js
@@ -1,2 +1,24 @@
 "use strict";
 
+//---Nav Stuff
+var navItems = [ //The items in the navbar
+    "Home", "About", "Browse", "Log In"
+];
+var navbar = $("#navbarItems");
+
+function fillNav (navItems){
+    var html = ' ';
+    navItems.forEach(function (navItem) {
+        html += '<li>' + '<a>';
+        html += navItem + '</a>' + '</li>';
+    });
+    return html;
+}
+
+navbar.html(fillNav(navItems));
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+//---Stories
+
+
--- web_game/web_games_js/profile.js
+++ web_game/web_games_js/profile.js
@@ -3,12 +3,15 @@
 var chatButton = document.getElementById("chat");
 
 function message(){
-    var html = document.getElementById("message").innerHTML;
+    var html = ' ';
     html += "<p>  Hey!  </p>";
+    return html;
 }
 
 function waitForMessage(timeInMilliseconds) {
     setTimeout(message, timeInMilliseconds);
 }
 
-chatButton.addEventListener('click', message);
+chatButton.addEventListener('click', function (ev) {
+    document.getElementById("message").innerHTML = message();
+});
